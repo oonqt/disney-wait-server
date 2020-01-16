a1a3cdd82687466c020b31a8b646bd39b58cbaba
oonqt <r0blox1053@outlook.com>
2020-01-15 17:58:20 -0800

    sort alphabetically, unnecessary force create

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { rideModel } = require("./models");
 const { PORT } = require("./config.json");
 
-const CA_API = new Themeparks.Parks.DisneylandResortCaliforniaAdventure({ forceCreate: true });
+const CA_API = new Themeparks.Parks.DisneylandResortCaliforniaAdventure();
 const DL_API = new Themeparks.Parks.DisneylandResortMagicKingdom({ resortId: 80008297, parkId: 330339 });
 
 app.use(cors({ methods: ["GET"] }));
@@ -19,9 +19,11 @@ app.get("/rideTimes", async (_, res) => {
         let waitTimesCA = await CA_API.GetWaitTimes();
         waitTimesCA = waitTimesCA.map(ride => {
             return rideModel(ride);
-        })
+        });
 
-        res.json([...waitTimesDL, ...waitTimesCA]);
+        const times = [...waitTimesDL, ...waitTimesCA].sort();
+
+        res.json(times);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal Server Error" });
